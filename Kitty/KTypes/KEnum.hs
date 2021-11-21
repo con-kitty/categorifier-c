ec5eec2990b1003b53e86f054b7f164811a0e8c4
Ziyang Liu <unsafeFixIO@gmail.com>
2021-11-20 19:31:52 -0800

    Add ktypes:enum

---
+++ Kitty/KTypes/KEnum.hs
@@ -0,0 +1,116 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
+-- | Treat 'Word8's as 'Enum's.
+module Kitty.KTypes.KEnum
+  ( KEnum (..),
+    toKEnum,
+  )
+where
+
+import Data.Functor.Compose (Compose (..))
+import Data.Proxy (Proxy (..))
+import Data.Word (Word8)
+import GHC.Generics (Generic)
+import Kitty.CExpr.Cat.TargetOb (TargetOb, TargetObTC1)
+import Kitty.CTypes.CGeneric.Class (CGeneric)
+import Kitty.CTypes.GArrays (GArrays)
+import Kitty.CTypes.Render (renderCxxType)
+import Kitty.CTypes.ToCxxType (ToCxxType (..))
+import Kitty.CTypes.Types (CEnum (..), CTypeF (..), CxxType (..))
+import Kitty.KTypes.BooleanLogic (KAnd)
+import Kitty.KTypes.Equality (KEq (..))
+import Kitty.KTypes.KLiteral (KLiteral, kliteral)
+import Kitty.KTypes.TotalOrder (KOrd (..))
+import Kitty.Plugin.Client (deriveHasRep)
+import PyF (fmt)
+
+-- | newtype wrapper around Word8 with functions for treating it as its phantom type
+-- TODO(greg): if we define this as:
+--   newtype KEnum a f = KEnum (f Word8)
+-- then do all our troubles go away?
+newtype KEnum f a = KEnum (f Word8) deriving (Generic)
+
+-- | Convert a 'KEnum' to a 'CEnum' by first creating a @'CEnum' 'Proxy'@ to get the type right, and
+-- then converting the 'Proxy' to a 'Functor' container type by taking the 'KEnum's 'Word8' and
+-- putting it into the @'CEnum' 'Proxy'@.
+-- TODO(greg): define 'toKEnum and 'fromKEnum in terms of this function.
+kenumToCEnum :: forall f g a. (ToCxxType f a, Functor g) => g (KEnum f a) -> CEnum (Compose g f)
+kenumToCEnum =
+  let enumCxxType :: CEnum (Compose Proxy f)
+      enumCxxType = case toCxxType (Proxy @a) of
+        CxxTypeCType (CTypeEnum cenum) -> cenum
+        otherType -> error [fmt|KEnum got a non-enum type {renderCxxType otherType}|]
+      getKEnumWord :: KEnum f a -> f Word8
+      getKEnumWord (KEnum x) = x
+      toEnumCxxType :: g (KEnum f a) -> CEnum (Compose g f)
+      toEnumCxxType kenum = enumCxxType {ceData = Compose word8}
+        where
+          word8 :: g (f Word8)
+          word8 = getKEnumWord <$> kenum
+   in toEnumCxxType
+
+instance CGeneric (KEnum f a)
+
+instance GArrays f (KEnum f a)
+
+instance
+  ( ToCxxType f a
+  ) =>
+  ToCxxType f (KEnum f a)
+  where
+  toCxxType = CxxTypeCType . CTypeEnum . kenumToCEnum
+
+deriveHasRep ''KEnum
+
+type instance TargetOb (KEnum f a) = KEnum (TargetObTC1 f) a
+
+instance (KAnd f, KEq f (f Word8)) => KEq f (KEnum f a) where
+  KEnum x .== KEnum y = x .== y
+
+  KEnum x ./= KEnum y = x ./= y
+
+instance (KOrd f (f Word8)) => KOrd f (KEnum f a) where
+  (.<) (KEnum x) (KEnum y) = (.<) x y
+
+  (.<=) (KEnum x) (KEnum y) = (.<=) x y
+
+  (.>) (KEnum x) (KEnum y) = (.>) x y
+
+  (.>=) (KEnum x) (KEnum y) = (.>=) x y
+
+  kMin (KEnum x) (KEnum y) = KEnum $ kMin x y
+
+  kMax (KEnum x) (KEnum y) = KEnum $ kMax x y
+
+-- | convert enum to 'KEnum'
+toKEnum ::
+  forall f a.
+  (Bounded a, Enum a, Eq a, KLiteral f Word8) =>
+  a ->
+  KEnum f a
+toKEnum x = either error (KEnum . kliteral) $ toIndex allEnums
+  where
+    allEnums :: [a]
+    allEnums = enumFrom minBound
+    maxK :: Word8
+    maxK = maxBound
+    toIndex :: [a] -> Either String Word8
+    toIndex =
+      maybe
+        (Left "enum doesn't match anything in enumFrom minBound")
+        ( \i ->
+            if i >= fromIntegral maxK
+              then Left $ "enum too big (" <> show maxK <> ")"
+              else pure $ fromIntegral i
+        )
+        . elemIndex' x
+
+    -- See https://kitty-hawk.atlassian.net/browse/SW-3503
+    elemIndex' e = go (0 :: Int)
+      where
+        go idx (y : ys)
+          | y == e = pure idx
+          | otherwise = go (idx + 1) ys
+        go _idx [] = Nothing
---
+++ Kitty/KTypes/SwitchCase.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE MonoLocalBinds #-}
+
+module Kitty.KTypes.SwitchCase
+  ( KIf (..),
+    switch,
+    switch',
+    unsafeIndex,
+    kIfThenElseChain,
+  )
+where
+
+import qualified Barbies
+import Data.Bool (bool)
+import Data.Functor.Compose (Compose (..))
+import qualified Data.Vector as V
+import Data.Word (Word8)
+import qualified Kitty.Common.IO.Exception as Exception
+import Kitty.KTypes.Conditional (KSelect (..))
+import Kitty.KTypes.Equality (KEq (..))
+import Kitty.KTypes.KEnum (KEnum (..))
+import Kitty.KTypes.KLiteral (KLiteral (..), true)
+import Kitty.PolyVec (PolyVec, pdevectorize, pvectorize)
+import Kitty.Prim (Arrays, IsPrimitive)
+
+class KSelect f => KIf f where
+  -- | implement `kIfThenElse` by converting a bool to a KEnum.
+  -- This is dangerous in general, but fine here.
+  -- This is the sole purpose for 'unsafeBoolToZeroOrOne'
+  kIfThenElse :: forall b. PolyVec f b => f Bool -> b -> b -> b
+  kIfThenElse boolIndex tru fls = switch enumIndex (bool fls tru)
+    where
+      enumIndex :: KEnum f Bool
+      enumIndex = KEnum (unsafeBoolToZeroOrOne boolIndex)
+
+switch ::
+  forall f a b.
+  (KSelect f, Bounded a, Enum a, PolyVec f b) =>
+  KEnum f a ->
+  (a -> b) ->
+  b
+switch (KEnum index) toBranch = unsafeIndex allOutputs index
+  where
+    allEnums :: [a]
+    allEnums = enumFrom minBound
+    allOutputs :: [b]
+    allOutputs = fmap toBranch allEnums
+
+-- | Version of 'switch' that chains 'kIfThenElse's and uses a default argument instead of doing an
+-- array index on all options. This is more efficient when you want to pattern match on a couple
+-- possibilities but accept the default most of the time.
+switch' :: forall a b f. (KEq f a, KIf f, PolyVec f b) => a -> [(a, b)] -> b -> b
+switch' inputSwitchIsLookingFor optionList default' = foldr matchOr default' optionList
+  where
+    matchOr :: (a, b) -> b -> b
+    matchOr (thisInput, thisOutput) earliestMatchThusFar =
+      kIfThenElse
+        (thisInput .== inputSwitchIsLookingFor)
+        -- Found an earlier match, so take it.
+        thisOutput
+        earliestMatchThusFar
+
+-- | Internal function for doing @select@-like operations.
+unsafeIndex :: forall f a. (KSelect f, PolyVec f a) => [a] -> f Word8 -> a
+unsafeIndex allOutputs0 index = either Exception.impureThrow id $ pdevectorize outputs
+  where
+    processOutputs :: [Arrays (Compose V.Vector f)] -> Arrays (Compose V.Vector f)
+    processOutputs = Barbies.bmapC @IsPrimitive selectPrimList . Barbies.bdistribute
+      where
+        selectPrimList ::
+          forall c.
+          IsPrimitive c =>
+          Compose [] (Compose V.Vector f) c ->
+          Compose V.Vector f c
+        selectPrimList (Compose allPrimOutputs) =
+          Compose . V.fromList $ selectList (V.toList . getCompose <$> allPrimOutputs) index
+    outputs :: Arrays (Compose V.Vector f)
+    outputs = either Exception.impureThrow processOutputs $ traverse pvectorize allOutputs0
+
+-- | Grabs first element of list which has a 'True' first element.
+-- If all are 'False' returns a default.
+kIfThenElseChain ::
+  forall a f.
+  (KEq f (f Bool), KLiteral f Bool, KIf f, PolyVec f a) =>
+  [(f Bool, a)] ->
+  a ->
+  a
+kIfThenElseChain = switch' true
